20a5df138c5bf353e21e1f5e3055635dcf028bcf
sonu85336 <sonukushwaha098@gmail.com>
2022-11-23 14:15:08 +0530

    edit button

--- bookinappoint.js
+++ bookinappoint.js
@@ -10,7 +10,17 @@ let li = document.createElement('li')
 li.className='list'
 li.appendChild(document.createTextNode(newItem))
 li.appendChild(document.createTextNode(newItem1))
+// add delet button
+var deletbtn = document.createElement('button')
+deletbtn.className ='btn btn-danger btn-sm float-right delete'
+deletbtn.appendChild(document.createTextNode('X'))
+li.appendChild(deletbtn)
 
+var addedit =document.createElement('button') 
+addedit.className ='btn btn-success btn-sm float-right active'
+addedit.appendChild(document.createTextNode('EDIT'))
+li.appendChild(addedit)
+//listadd.appendChild(li)
 }
 let arr=[]
 function savetolocal(event){
@@ -30,6 +40,28 @@ showNewUserOnScreen(mylocal)
 }
 function showNewUserOnScreen(user){
   const parentNode = document.getElementById('listadd')
-  const childhtml = `<li>${user.user}  ${user.email}`
+  const childhtml = `<li id=${user.email}>${user.user}  ${user.email}     
+  <button onclick = deletUser('${user.email}')>DELETE</button>
+  <button onclick=editUser('${user.user}','${user.email}')>EDIT</button>
+  
+  </li>` 
       parentNode.innerHTML= parentNode.innerHTML+childhtml;
 }
+//edit user 
+function editUser(name,emailId){ 
+  document.getElementById('username').value=name;
+  document.getElementById('email').value=emailId;
+ 
+  deletUser(emailId)
+}
+//delete user
+function deletUser(emailId){
+  localStorage.removeItem(emailId);
+  removeUserFromScreen(emailId);
+}
+function removeUserFromScreen(emailId){
+  const parentNode =document.getElementById('listadd')
+  const childnodedelet=document.getElementById(emailId)
+  parentNode.removeChild(childnodedelet)
+}
+/*************************/
